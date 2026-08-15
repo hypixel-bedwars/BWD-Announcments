736942275d63cb6374e32ba018e3ad7ae08983d7
VihaanReddyM <vihaanreddy08@gmail.com>
2026-08-15 18:15:25 +0530

    Update guildMemberUpdate.ts

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -30,7 +30,6 @@ export default {
           `Thank you, ${newMember} — you've boosted the server!\n\n` +
             `> ### **Boost Count**\n` +
             `> This is boost #${newMember.guild.premiumSubscriptionCount ?? "?"} for the server\n` +
-            `> \n` +
             `> ### **Exclusive Perks**\n` +
             `> • You get the <@&${config.discordBoostRoleId}> role\n` +
             `> • Access to wool colors\n` +
